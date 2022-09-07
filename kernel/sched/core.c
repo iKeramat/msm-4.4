db4198050df1cf7a809f9b065f3425e96a3778d0
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-09-07 19:18:12 +0430

    temp fix

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5429,9 +5429,9 @@ void show_state_filter(unsigned long state_filter)
 
 	touch_all_softlockup_watchdogs();
 
-#ifdef CONFIG_SCHED_DEBUG
-	sysrq_sched_debug_show();
-#endif
+//#ifdef CONFIG_SCHED_DEBUG
+//	sysrq_sched_debug_show();
+//#endif
 	rcu_read_unlock();
 	/*
 	 * Only show locks if all tasks are dumped:
